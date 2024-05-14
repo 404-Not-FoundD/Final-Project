bf1fca39bed35d50af7b76bcd3359c198b229b29
ga ga <sychen16866@gmail.com>
2024-05-14 22:56:37 +0800

    sixth commit

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -5,14 +5,20 @@ public class MonsterMovement : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private PlayerManager playerManager;
 
-    public float speed = 1f;
-
+    public float speed = 0.8f;
+    private float scale = 0.3f;
     private void FixedUpdate()
     {
         var playerPosition = playerManager.Position;
         var position = transform.position;
         var direction = (playerPosition - position).normalized;
         var targetPosition = position + new Vector3(direction.x, 0f, 0f); // Movement only on x-axis
+        if(direction.x<0){
+            transform.localScale = new Vector3(-scale, scale, scale);
+        }
+        else{
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
         rb.MovePosition(Vector3.MoveTowards(position, targetPosition, speed * Time.fixedDeltaTime));
     }
 }
