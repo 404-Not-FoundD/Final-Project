266425f9298aafbcdfa69745d2be6a94bb226b2a
ga ga <sychen16866@gmail.com>
2024-05-15 12:44:32 +0800

    7th commit

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -4,7 +4,7 @@ public class MonsterMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private PlayerManager playerManager;
-
+    [SerializeField] private float distance_to_chase = 0.9f;
     public float speed = 0.8f;
     private float scale = 0.3f;
     private void FixedUpdate()
@@ -13,12 +13,17 @@ public class MonsterMovement : MonoBehaviour
         var position = transform.position;
         var direction = (playerPosition - position).normalized;
         var targetPosition = position + new Vector3(direction.x, 0f, 0f); // Movement only on x-axis
-        if(direction.x<0){
-            transform.localScale = new Vector3(-scale, scale, scale);
+
+        if(math.abs(playerPosition.x - position.x) > distance_to_chase){
+
+            if(direction.x<0){//change the way monster look at 
+                transform.localScale = new Vector3(-scale, scale, scale);
+            }
+            else{
+                transform.localScale = new Vector3(scale, scale, scale);
+            }
+            
+            rb.MovePosition(Vector3.MoveTowards(position, targetPosition, speed * Time.fixedDeltaTime));//for chase
         }
-        else{
-            transform.localScale = new Vector3(scale, scale, scale);
-        }
-        rb.MovePosition(Vector3.MoveTowards(position, targetPosition, speed * Time.fixedDeltaTime));
     }
 }
